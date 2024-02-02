12dc463f9f915ff1354316bc1fc7e74b5b62602b
MaxwellChalmers <156827512+MaxwellChalmers@users.noreply.github.com>
2024-02-02 01:01:34 -0500

    Update OneLoop.java
    .

--- java/OneLoop.java
+++ java/OneLoop.java
@@ -68,7 +68,7 @@ public static void main(String[] args){
 
 	Integer[] t1 = {1,1,1,1,2,4,3};
 	Integer[] t2 = {1,2,4,5,6,7,8,9,135};
-    I   Integer[] t3 = {0}; 
+        Integer[] t3 = {0}; 
 	
 	System.out.println(Arrays.toString(t1));
 	System.out.println(twoOfAKindNested(Arrays.asList(t1)));
